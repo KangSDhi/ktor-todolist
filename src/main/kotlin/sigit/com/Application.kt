020927c5185bc087c011228f65a85e421fef92fe
KangSDhi <kangteknisi@gmail.com>
2023-01-17 10:39:32 +0700

    ToDo-Model, Ktor Features, Json Responses

--- src/main/kotlin/sigit/com/Application.kt
+++ src/main/kotlin/sigit/com/Application.kt
@@ -1,9 +1,17 @@
 package sigit.com
 
 import io.ktor.server.application.*
+import io.ktor.server.application.*
+import io.ktor.server.application.*
+import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import io.ktor.server.plugins.callloging.*
+import io.ktor.server.request.*
+import org.slf4j.event.*
 import sigit.com.plugins.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.serialization.gson.*
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
@@ -11,5 +19,14 @@ fun main() {
 }
 
 fun Application.module() {
+    install(CallLogging) {
+        level = Level.INFO
+        filter { call -> call.request.path().startsWith("/") }
+    }
+    install(ContentNegotiation){
+        gson{
+            setPrettyPrinting()
+        }
+    }
     configureRouting()
 }
--- src/main/kotlin/sigit/com/plugins/Routing.kt
+++ src/main/kotlin/sigit/com/plugins/Routing.kt
@@ -3,15 +3,23 @@ package sigit.com.plugins
 import io.ktor.server.routing.*
 import io.ktor.server.response.*
 import io.ktor.server.application.*
+import sigit.com.entities.ToDo
 
 fun Application.configureRouting() {
     routing {
+
+        val todos = listOf<ToDo>(
+            ToDo(1, "Place content for video #2", true),
+            ToDo(2, "Record video #2", false),
+            ToDo(3, "Upload video #2", false)
+        )
+
         get("/") {
             call.respondText("Hello Todolist!")
         }
 
         get("/todos"){
-
+            call.respond(todos)
         }
 
         get("/todos/{id}"){
